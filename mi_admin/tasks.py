c3fe63084f3f0e72a237b56da55a74338a88c55f
zask99 <www.fcalle@gmail.com>
2025-11-12 21:15:58 -0400

    Escribe aquí qué cambios hiciste

--- mi_admin/tasks.py
+++ mi_admin/tasks.py
@@ -1,7 +1,6 @@
 from celery import shared_task
 from mi_admin.models import Trimestre, Curso, Materia, Asignacion, ResumenTrimestral
 from .views.selenium_logic import ejecutar_bot_sie # El bot real
-
 @shared_task(bind=True)
 def iniciar_subida_sie_task(self, trimestre_id, curso_id, materia_id, username_sie, password_sie):
     try:
@@ -21,7 +20,7 @@ def iniciar_subida_sie_task(self, trimestre_id, curso_id, materia_id, username_s
         # 3. Preparar datos simples para el bot
         datos_simples = [
             {
-                'ci': res.estudiante.persona.ci,
+                'ci': res.estudiante.persona.carnet,  # <--- CORREGIDO AQUÍ
                 'nombre': res.estudiante.persona.get_full_name(),
                 'nota_final': res.nota_final_trimestre 
             }
@@ -33,7 +32,6 @@ def iniciar_subida_sie_task(self, trimestre_id, curso_id, materia_id, username_s
             username=username_sie,
             password=password_sie,
             notas=datos_simples,
-            # Pasa los nombres para que el bot pueda buscar
             curso_nombre=asignacion.curso.get_full_name(),
             materia_nombre=asignacion.materia.nombre
         )
